5c69548ccf83c74f00a2063ce7a759fc1afe8e97
Vantack <131598806+Vantack@users.noreply.github.com>
2023-05-05 00:47:16 +0300

    added attack bar UI element

--- DNDer/Assets/Scripts/attackBar.cs
+++ DNDer/Assets/Scripts/attackBar.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class attackBar : MonoBehaviour
 {
-    public float speed = 1f;
-    public float minX ;
-    public float maxX ;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float minX ;
+    [SerializeField] float maxX ;
+    [SerializeField] RectTransform hitter;
+    [SerializeField] Button button;
+    bool stopped = false;
 
-    private RectTransform rectTransform;
+    private float dir = 1f;
 
     void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
+        hitter.anchoredPosition = new Vector2(minX, hitter.anchoredPosition.y);
     }
 
     void Update()
     {
-        
+        if (!stopped) { 
+            hitter.anchoredPosition += new Vector2(dir * speed, 0f);
+            if (hitter.anchoredPosition.x > maxX || hitter.anchoredPosition.x < minX) dir *= -1f;
+        }
+    }
+
+    public void Stop()
+    {
+        stopped = true;
     }
 }
