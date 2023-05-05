c1c8106046ed208691d326199dd666966f238013
Emibeanie <emily.gendelev@gmail.com>
2023-05-05 14:27:49 +0300

    working on texture changes
    has a bug and changes location when button pressed

--- DNDer/Assets/Scripts/TinderController.cs
+++ DNDer/Assets/Scripts/TinderController.cs
@@ -8,16 +8,25 @@ public class TinderController : MonoBehaviour
     public SpriteRenderer mySprite;
     public Texture2D[] textures;
     public int currentTextureIndex = 0;
-        public void ChangeTexture()
-        {
+
+    public void ChangeTexture()
+    {
         currentTextureIndex++;
         if (currentTextureIndex >= textures.Length)
         {
             currentTextureIndex = 0;
         }
         Texture2D newTexture = textures[currentTextureIndex];
-        Sprite newSprite = Sprite.Create(newTexture, new Rect(0, 0, newTexture.width, newTexture.height), Vector2.zero);
+        Sprite originalSprite = mySprite.sprite;
+        Vector2 pivot = new Vector2(originalSprite.rect.x / originalSprite.rect.width, originalSprite.rect.y / originalSprite.rect.height);
+        Sprite newSprite = Sprite.Create(newTexture, originalSprite.rect, pivot);
+
         mySprite.sprite = newSprite;
-        mySprite.transform.position = new Vector3((float)-19.3901997, (float)7.7020998, (float)-0.922304749);
+        
+        //Sprite newSprite = Sprite.Create(newTexture, new Rect(0, 0, newTexture.width, newTexture.height), Vector2.zero);
+        //Vector2 originalPivot = mySprite.sprite.pivot;
+        //newSprite.pivot = originalPivot;
+        //mySprite.sprite = newSprite;
+        //mySprite.transform.position = new Vector3((float)-19.3901997, (float)7.7020998, (float)-0.922304749);
     }
 }
