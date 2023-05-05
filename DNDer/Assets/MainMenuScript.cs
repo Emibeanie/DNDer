14bab9ca85a61573a8d636ff2f965a47ad69d101
Emibeanie <emily.gendelev@gmail.com>
2023-05-05 22:16:09 +0300

    font

--- DNDer/Assets/MainMenuScript.cs
+++ DNDer/Assets/MainMenuScript.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public AudioMixer mainMixer;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -12,5 +14,14 @@ public class MainMenuScript : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("volume", volume);
+    }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
