f00915dad86d614cf3bcb004f5cc66ceb9201ed0
Emibeanie <emily.gendelev@gmail.com>
2023-05-06 05:46:23 +0300

    dinder with profiles

--- DNDer/Assets/Scripts/OptionsMenu.cs
+++ DNDer/Assets/Scripts/OptionsMenu.cs
@@ -6,6 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public AudioMixer mainMixer;
+    public AudioClip LobbyMusic;
+    public AudioSource Source;
+
+    public void Start()
+    {
+        Source.clip = LobbyMusic;
+        Source.Play();
+    }
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("volume", volume);
+    }
+
     public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
